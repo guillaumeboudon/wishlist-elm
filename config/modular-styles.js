f3c48df34b4d7b74ab1fa777bfb1471cc022644c
Guillaume BOUDON <guillaumeboudon@gmail.com>
2019-12-24 16:57:10 +0100

    :wrench: Set modular-styles up

---
+++ config/modular-styles.js
@@ -0,0 +1,8 @@
+module.exports = {
+  filesPath: 'src/css',
+  destPath: 'build/elm/Styles',
+  bundlePath: 'build/',
+  bundleName: 'bundle.css',
+  language: 'elm',
+  sourcePath: 'build/elm/',
+}
