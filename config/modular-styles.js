1b7fc6f926c322b66f81991c5b45209f8b4a0c5a
Guillaume BOUDON <guillaumeboudon@gmail.com>
2019-12-24 17:04:43 +0100

    :wrench: Set ESLint up

--- config/modular-styles.js
+++ config/modular-styles.js
@@ -1,3 +1,5 @@
+/*eslint-env node*/
+
 module.exports = {
   filesPath: 'src/css',
   destPath: 'build/elm/Styles',
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/*eslint-env node*/
+
 // Dependencies
 const path = require("path")
 const merge = require("webpack-merge")
