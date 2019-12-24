029c2f1a5c9a591dd515ff3a79d0d8de4e5857b9
Guillaume BOUDON <guillaumeboudon@gmail.com>
2019-12-24 17:36:19 +0100

    :sparkles: Use Browser.application

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,15 +1,80 @@
 module Main exposing (main)
 
 import Browser
-import Html
+import Html exposing (Html)
 import Html.Attributes as A
 import Styles.Global
 
 
-main : Program () String msg
+
+-- =============================================================================
+-- > Init
+-- =============================================================================
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello, World!", Cmd.none )
+
+
+
+-- =============================================================================
+-- > Update
+-- =============================================================================
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- =============================================================================
+-- > View
+-- =============================================================================
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Wishlist"
+    , body = [ Html.p [ A.class Styles.Global.greenText ] [ Html.text model ] ]
+    }
+
+
+
+-- =============================================================================
+-- > Subscriptions
+-- =============================================================================
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- =============================================================================
+-- > Main
+-- =============================================================================
+
+
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = "Hello, elm!"
-        , update = \_ v -> v
-        , view = \v -> Html.p [ A.class Styles.Global.greenText ] [ Html.text v ]
+    Browser.application
+        { init = \_ _ _ -> init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = always NoOp
+        , onUrlRequest = always NoOp
         }
